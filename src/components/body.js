3ad76feac5124d624a583c2d212ac182bbf7e16f
JoshuaAN <joshn@team2363.org>
2024-02-03 09:06:15 -0500

    Second commit

---
+++ src/components/body.js
@@ -0,0 +1,12 @@
+import './body.css';
+import Dashboard from './pages/dashboard'
+
+function Body() {
+  return (
+    <div className="body">
+        <Dashboard></Dashboard>
+    </div>
+  );
+}
+
+export default Body;
